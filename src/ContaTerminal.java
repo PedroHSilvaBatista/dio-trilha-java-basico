6a5a04fbcd4724bc1c0561fb2a2e4ea8db9f6cb0
Pedro <pedrohenriquesb02@gmail.com>
2025-03-01 21:24:36 -0300

    Primeiro commit

---
+++ src/ContaTerminal.java
@@ -0,0 +1,12 @@
+public class ContaTerminal {
+    public static void main(String[] args) {
+        // TODO: Conhecer e importar a classe Scanner
+
+        // Exibir as mensagens para o usuário
+
+        // Obter pela classe Scanner os valores digitados no terminal
+
+        // Exibir a mensagem da conta criada
+
+    }
+}
